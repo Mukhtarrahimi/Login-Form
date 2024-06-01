00636694e4b7ac0a74a270e7d9597e6dc7bfb5ed
Mukhtarrahimi <mukhtarrahimi110@gmail.com>
2024-06-01 11:15:38 +0430

    javascript code updated

--- script/app.js
+++ script/app.js
@@ -2,12 +2,14 @@ const title = document.getElementById("title");
 const nameField = document.getElementById("nameField");
 const signinBtn = document.getElementById("signinBtn");
 const signupBtn = document.getElementById("signupBtn");
+const forgotPass = document.getElementById('forgotPass');
 
 signinBtn.onclick = () => {
   title.innerHTML = "Sign In";
   nameField.style.maxHeight = "0px";
   signinBtn.classList.remove("disable");
   signupBtn.classList.add("disable");
+  forgotPass.style.display = "block";
 };
 
 
@@ -16,4 +18,5 @@ signupBtn.onclick = () => {
     nameField.style.maxHeight = "60px";
     signinBtn.classList.add("disable");
     signupBtn.classList.remove("disable");
+    forgotPass.style.display = "none";
 }
